c2f641dab1dbf68102152f16581080c78a9db969
Ali Rosi <alirosi3214@gmail.com>
2023-12-04 20:06:01 +0700

    fix API & add search for movie

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import "./App.css";
-import { getMovieList } from "./api";
+import { getMovieList, searchMovie } from "./api";
 import { useEffect, useState } from "react";
 
 const App = () => {
@@ -16,27 +16,35 @@ const App = () => {
       return (
         <div className="Movie-wrapper" key={i}>
           <div className="Movie-tittle">{movie.title}</div>
-          <img className="Movie-image" src={movie.poster_path} alt="" />
-          <div className="Movie-date">{movie.release_date}</div>
+          <img
+            className="Movie-image"
+            src={`${process.env.REACT_APP_BASEIMGURL}/${movie.poster_path}`}
+            alt=""
+          />
+          <div className="Movie-date">release: {movie.release_date}</div>
           <div className="Movie-rate">{movie.vote_average}</div>
         </div>
       );
     });
   };
 
-  const search = (q) => {
-    console.log({ q });
+  const search = async (q) => {
+    if (q.length > 3) {
+      const query = await searchMovie(q);
+      setPopularMovies(query.results);
+      console.log({ query: query });
+    }
   };
 
   return (
     <div className="App">
       <header className="App-header">
-        <h1>MOVIE APP 77</h1>
+        <h1>MOVIE 77</h1>
         <input
-          placeholder="cari film kesayangan..."
+          placeholder="search movie..."
           className="Movie-search"
           onChange={({ target }) => search(target.value)}
-        ></input>
+        />
         <div className="Movie-container">
           <PopularMoviesList />
         </div>
